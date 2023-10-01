1486dbc9f9ab07f0ae5b85c17bb86a9fc112f7f2
Mykhailo Briazkalo <mihailbzkl@gmail.com>
2023-10-01 11:14:34 -0400

    stock change

---
+++ Assets/spritechanger.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spritechanger : MonoBehaviour
+{
+    public Sprite image1;
+    // Update is called once per frame
+    void Update()
+    {
+        if (moneyTracker.companyTwo >= 3)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = image1;
+            earthxstock.earthxValue -= 2000;
+            moneyTracker.stockvalue -= (2000 * moneyTracker.companyTwo);
+            earthxstock.UpdateText();
+            moneyTracker.UpdateText();
+        }
+    }
+}
